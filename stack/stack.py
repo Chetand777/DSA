b0d2ade6011cb46abd12012cfd97f0477e850655
Chetand777 <chetandaddikar31@gmail.com>
2024-07-23 19:41:12 +0530

    stack is added

--- stack/stack.py
+++ stack/stack.py
@@ -42,3 +42,34 @@ class ArrayStack:
       raise Empty("Stack is empty")
     return self._data.pop() # Remove and return the last element of the list
   
+
+# Algorithm to match valid parenthesis
+
+def is_matched(expression):
+   
+  bracket_pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+   
+  S = ArrayStack()
+  for bracket in expression:
+    if bracket in bracket_pairs.values():
+      S.push(bracket)
+    elif bracket in bracket_pairs.keys():
+      if S.is_empty():
+        return False
+      if bracket_pairs[bracket] != S.pop():
+        return False
+  return S.is_empty()
+
+# Test case
+
+expression1 = "((( )(( )){([( )])}))"
+
+expression2 = " ({[])}"
+
+print(is_matched(expression2))  
+
+  
